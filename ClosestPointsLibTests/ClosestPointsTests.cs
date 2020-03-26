3380fc9130e1141fab1cd64bbd44349644f649d5
DevinDow <DevinDow@gmail.com>
2020-03-26 15:36:26 -0700

    add UnitTesting

---
+++ ClosestPointsLibTests/ClosestPointsTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClosestPointsLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClosestPointsLib.Tests
+{
+    [TestClass()]
+    public class ClosestPointsTests
+    {
+        [TestMethod()]
+        public void FindClosestPointsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
